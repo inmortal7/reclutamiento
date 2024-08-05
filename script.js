8ed14f4d410317f99353356dbe80621428f6c051
inmortal7 <johandavid7061@gmail.com>
2024-08-04 23:07:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('recruitmentForm').addEventListener('submit', function(event) {
+    // Aquí puedes agregar validaciones adicionales si es necesario
+    event.preventDefault(); // Evita el envío del formulario para pruebas
+    alert('Formulario enviado correctamente.');
+});
